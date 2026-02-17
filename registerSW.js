c31b945d4da8a9a64d1396ff84dd9ef82903dd7b
Pramod Karandikar <pramodkd22@gmail.com>
2026-02-17 16:58:31 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/german-tutor/sw.js', { scope: '/german-tutor/' })})}
